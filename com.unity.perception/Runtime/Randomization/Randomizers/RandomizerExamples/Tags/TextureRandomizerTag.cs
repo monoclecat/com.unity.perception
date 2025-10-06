b5b925faba5c9c3d438a4bae97845049cf8c1bec
Andrew D <git@andrewdelay.com>
2025-10-06 12:45:18 +0200

    Add component requirement for a Renderer like ColorRandomizerTag needs it

--- com.unity.perception/Runtime/Randomization/Randomizers/RandomizerExamples/Tags/TextureRandomizerTag.cs
+++ com.unity.perception/Runtime/Randomization/Randomizers/RandomizerExamples/Tags/TextureRandomizerTag.cs
@@ -3,6 +3,7 @@ namespace UnityEngine.Perception.Randomization.Randomizers.Tags
     /// <summary>
     /// Used in conjunction with a TextureRandomizer to vary the material texture of GameObjects
     /// </summary>
+    [RequireComponent(typeof(Renderer))]
     [AddComponentMenu("Perception/RandomizerTags/Texture Randomizer Tag")]
     public class TextureRandomizerTag : RandomizerTag {}
 }
